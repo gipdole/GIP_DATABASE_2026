3bb6e6cbd7d1aa30dda698ff7996dbed13ff8261
pepperonipowered <pepperonipowered@gmail.com>
2026-02-18 10:58:52 +0800

    fixed text wrapping for view employee pdf

--- src/utils/fillPdf.js
+++ src/utils/fillPdf.js
@@ -1,6 +1,6 @@
 import { border, borderColor, fontSize } from "@mui/system";
 import { PDFDocument, rgb, StandardFonts } from "pdf-lib";
-import {employee_info} from "./gip_view_employee_pdf.js";
+import { employee_info } from "./gip_view_employee_pdf.js";
 /**
  * Helper to draw wrapped text in a pseudo text box.
  */
@@ -77,381 +77,7 @@ export async function fillGIPInfoPDF(pdfUrl, data) {
     const checkFont = await pdfDoc.embedFont(StandardFonts.ZapfDingbats);
     const { width, height } = page.getSize();
 
-     drawEmployeeInfo(page, data, font, checkFont, height);
-
-    /*
-        const genderCheckBoxPos = {
-        male: { x: 144, y: height - 308 },
-        female: { x: 231, y: height - 308 },
-    };
-    const gender = data.gender ? data.gender.toLowerCase() : "";
-    const checkSize = 14;
-    switch (gender) {
-        case "male":
-            page.drawText("X", {
-                x: genderCheckBoxPos.male.x,
-                y: genderCheckBoxPos.male.y,
-                size: checkSize,
-                font,
-                color: rgb(0, 0, 0),
-            });
-            break;
-        case "female":
-            page.drawText("X", {
-                x: genderCheckBoxPos.female.x,
-                y: genderCheckBoxPos.female.y,
-                size: checkSize,
-                font,
-                color: rgb(0, 0, 0),
-            });
-            break;
-        default:
-            break;
-    }
-
-    const civilStatusCheckboxPos = {
-        single: { x: 144, y: height - 328 },
-        married: { x: 231, y: height - 326 },
-        widowed: { x: 326, y: height - 328 },
-    };
-
-    const civilStatus = data.civilStatus ? data.civilStatus.toLowerCase() : "";
-    switch (civilStatus) {
-        case "single":
-            page.drawText("X", {
-                x: civilStatusCheckboxPos.single.x,
-                y: civilStatusCheckboxPos.single.y,
-                size: checkSize,
-                font,
-                color: rgb(0, 0, 0),
-            });
-            break;
-        case "married":
-            page.drawText("X", {
-                x: civilStatusCheckboxPos.married.x,
-                y: civilStatusCheckboxPos.married.y,
-                size: checkSize,
-                font,
-                color: rgb(0, 0, 0),
-            });
-            break;
-        case "widowed":
-            page.drawText("X", {
-                x: civilStatusCheckboxPos.widowed.x,
-                y: civilStatusCheckboxPos.widowed.y,
-                size: checkSize,
-                font,
-                color: rgb(0, 0, 0),
-            });
-            break;
-    }
-    const name = data.fullName ? data.fullName.toUpperCase() : "";
-    page.drawText(name, {
-        x: 45,
-        y: height - 157,
-        size: 8,
-        font,
-        color: rgb(0, 0, 0),
-    });
-    page.drawText(data.address, {
-        x: 45,
-        y: height - 205,
-        size: 8,
-        font,
-        color: rgb(0, 0, 0),
-    });
-    page.drawText(data.contactNumber, {
-        x: 90,
-        y: height - 247,
-        size: 8,
-        font,
-        color: rgb(0, 0, 0),
-    });
-    page.drawText(data.email, {
-        x: 110,
-        y: height - 261,
-        size: 8,
-        font,
-        color: rgb(0, 0, 0),
-    });
-    page.drawText(data.birthDate, {
-        x: 200,
-        y: height - 292,
-        size: 8,
-        font,
-        color: rgb(0, 0, 0),
-    });
-
-    page.drawText(data.collegeSchool, {
-        x: 40,
-        y: height - 392,
-        size: 8,
-        font,
-        color: rgb(0, 0, 0),
-    });
-    page.drawText(data.collegeYear, {
-        x: 345,
-        y: height - 392,
-        size: 8,
-        font,
-        color: rgb(0, 0, 0),
-    });
-    page.drawText(data.collegeDegree, {
-        x: 409,
-        y: height - 392,
-        size: 8,
-        font,
-        color: rgb(0, 0, 0),
-    });
-    page.drawText(data.seniorHighSchool, {
-        x: 40,
-        y: height - 410,
-        size: 8,
-        font,
-        color: rgb(0, 0, 0),
-    });
-    page.drawText(data.seniorHighYear, {
-        x: 345,
-        y: height - 410,
-        size: 8,
-        font,
-        color: rgb(0, 0, 0),
-    });
-    page.drawText(data.seniorHighDegree, {
-        x: 409,
-        y: height - 410,
-        size: 8,
-        font,
-        color: rgb(0, 0, 0),
-    });
-    page.drawText(data.secondarySchool, {
-        x: 40,
-        y: height - 428,
-        size: 8,
-        font,
-        color: rgb(0, 0, 0),
-    });
-    page.drawText(data.secondaryYear, {
-        x: 345,
-        y: height - 428,
-        size: 8,
-        font,
-        color: rgb(0, 0, 0),
-    });
-    page.drawText(data.secondaryDegree, {
-        x: 409,
-        y: height - 428,
-        size: 8,
-        font,
-        color: rgb(0, 0, 0),
-    });
-    page.drawText(data.primarySchool, {
-        x: 40,
-        y: height - 448,
-        size: 8,
-        font,
-        color: rgb(0, 0, 0),
-    });
-    page.drawText(data.primaryYear, {
-        x: 345,
-        y: height - 448,
-        size: 8,
-        font,
-        color: rgb(0, 0, 0),
-    });
-    page.drawText(data.primaryDegree, {
-        x: 409,
-        y: height - 448,
-        size: 8,
-        font,
-        color: rgb(0, 0, 0),
-    });
-
-    page.drawText(data.workCompany, {
-        x: 40,
-        y: height - 495,
-        size: 8,
-        font,
-        color: rgb(0, 0, 0),
-    });
-    page.drawText(data.workPosition, {
-        x: 220,
-        y: height - 495,
-        size: 8,
-        font,
-        color: rgb(0, 0, 0),
-    });
-    page.drawText(data.workPeriod, {
-        x: 409,
-        y: height - 495,
-        size: 8,
-        font,
-        color: rgb(0, 0, 0),
-    });
-
-    if (data.pwd) {
-        page.drawText("X", {
-            x: 186,
-            y: height - 547,
-            size: 12,
-            font,
-            color: rgb(0, 0, 0),
-        });
-    }
-    if (data.iP) {
-        page.drawText("X", {
-            x: 237,
-            y: height - 547,
-            size: 12,
-            font,
-            color: rgb(0, 0, 0),
-        });
-    }
-    if (data.victimOfArmedConflict) {
-        page.drawText("X", {
-            x: 277,
-            y: height - 547,
-            size: 12,
-            font,
-            color: rgb(0, 0, 0),
-        });
-    }
-    if (data.rebelReturnee) {
-        page.drawText("X", {
-            x: 404,
-            y: height - 547,
-            size: 12,
-            font,
-            color: rgb(0, 0, 0),
-        });
-    }
-    if (data.fourP) {
-        page.drawText("X", {
-            x: 255,
-            y: height - 566,
-            size: 12,
-            font,
-            color: rgb(0, 0, 0),
-        });
-    }
-    if (data.othersDG) {
-        page.drawText(data.othersDG, {
-            x: 340,
-            y: height - 564,
-            size: 8,
-            font,
-            color: rgb(0, 0, 0),
-        });
-    }
-
-    page.drawText(data.emergencyName, {
-        x: 80,
-        y: height - 705,
-        size: 9,
-        font,
-        color: rgb(0, 0, 0),
-    });
-    page.drawText(data.emergencyContact, {
-        x: 115,
-        y: height - 720,
-        size: 9,
-        font,
-        color: rgb(0, 0, 0),
-    });
-    page.drawText(data.emergencyAddress, {
-        x: 85,
-        y: height - 735,
-        size: 8,
-        font,
-        color: rgb(0, 0, 0),
-    });
-    page.drawText(data.gsisName, {
-        x: 390,
-        y: height - 704,
-        size: 9,
-        font,
-        color: rgb(0, 0, 0),
-    });
-    page.drawText(data.gsisRelationship, {
-        x: 360,
-        y: height - 720,
-        size: 9,
-        font,
-        color: rgb(0, 0, 0),
-    });
-
-    if (data.birthCertificate) {
-        page.drawText("X", {
-            x: 50,
-            y: height - 855,
-            size: 12,
-            font,
-            color: rgb(0, 0, 0),
-        });
-    }
-    if (data.transcriptOfRecords) {
-        page.drawText("X", {
-            x: 50,
-            y: height - 870,
-            size: 12,
-            font,
-            color: rgb(0, 0, 0),
-        });
-    }
-    if (data.barangayCertificate) {
-        page.drawText("X", {
-            x: 50,
-            y: height - 886,
-            size: 12,
-            font,
-            color: rgb(0, 0, 0),
-        });
-    }
-    if (data.form137138) {
-        page.drawText("X", {
-            x: 224,
-            y: height - 855,
-            size: 12,
-            font,
-            color: rgb(0, 0, 0),
-        });
-    }
-    if (data.diploma) {
-        page.drawText("X", {
-            x: 224,
-            y: height - 870,
-            size: 12,
-            font,
-            color: rgb(0, 0, 0),
-        });
-    }
-    if (data.othersD) {
-        page.drawText("X", {
-            x: 224,
-            y: height - 886,
-            size: 12,
-            font,
-            color: rgb(0, 0, 0),
-        });
-    }
-    page.drawText(data.othersD, {
-        x: 280,
-        y: height - 886,
-        size: 8,
-        font,
-        color: rgb(0, 0, 0),
-    });
-    if (data.othersD) {
-        page.drawText("X", {
-            x: 432,
-            y: height - 855,
-            size: 12,
-            font,
-            color: rgb(0, 0, 0),
-        });
-    }
-    */
-
+    drawEmployeeInfo(page, data, font, checkFont, height);
     const pdfBytes = await pdfDoc.save();
     return new Blob([pdfBytes], { type: "application/pdf" });
 }
@@ -488,8 +114,8 @@ function drawTextInFrame(page, text, frame, options = {}) {
     }
 
     // Calculate content area (frame minus padding)
-    const contentWidth = width - (padding * 2);
-    const contentHeight = height - (padding * 2);
+    const contentWidth = width - padding * 2;
+    const contentHeight = height - padding * 2;
     const contentX = x + padding;
 
     // Wrap text into lines
@@ -510,7 +136,7 @@ function drawTextInFrame(page, text, frame, options = {}) {
 
     // Draw each line
     lines.forEach((line, index) => {
-        const yPosition = startY - (index * lineHeight);
+        const yPosition = startY - index * lineHeight;
 
         // Only draw if within frame bounds
         if (yPosition >= y && yPosition <= y + height) {
@@ -530,9 +156,9 @@ function drawTextInFrame(page, text, frame, options = {}) {
 }
 
 function wrapText(text, font, fontSize, maxWidth) {
-    const words = text.split(' ');
+    const words = text.split(" ");
     const lines = [];
-    let currentLine = '';
+    let currentLine = "";
 
     for (const word of words) {
         const testLine = currentLine ? `${currentLine} ${word}` : word;
@@ -575,8 +201,12 @@ function alignText(text, font, fontSize, startX, maxWidth, align) {
 }
 
 function drawEmployeeInfo(page, data, font, checkFont, height) {
-    employee_info.forEach(field => {
-        if (field.id === "gender" || field.id === "civilStatus") {
+    employee_info.forEach((field) => {
+        const hasPositions = field.genderPos || field.civilStatusPos;
+        const isSimpleCheckbox = !field.width && field.x && field.y;
+        const othersDCheck = field.id === "othersDCheck" && data.othersDG;
+
+        if (hasPositions || isSimpleCheckbox || othersDCheck) {
             handleSpecialField(page, data, field, checkFont, height);
             return;
         }
@@ -597,30 +227,37 @@ function drawEmployeeInfo(page, data, font, checkFont, height) {
                 font: font,
                 fontSize: field.size,
                 padding: field.padding || 0,
-                drawBorder: field.drawBorder || false,
+                drawBorder: false, //field.drawBorder || false
                 align: field.align || "left",
                 verticalAlign: field.verticalAlign || "middle",
                 padding: field.padding ?? 0,
                 color: rgb(0, 0, 0),
-                drawBorder: true,
-            }
+            },
         );
     });
 }
 
 function handleSpecialField(page, data, field, checkFont, height) {
-    if (field.id === "gender") {
+    if (field.genderPos) {
         const genderValue = data.gender?.toLowerCase();
         const pos = field.genderPos[genderValue];
         if (pos) {
             drawCheckmark(page, pos.x, height - pos.y, checkFont, field.size);
         }
-    } else if (field.id === "civilStatus") {
+    }
+    if (field.civilStatusPos) {
         const statusValue = data.civilStatus?.toLowerCase();
         const pos = field.civilStatusPos[statusValue];
         if (pos) {
             drawCheckmark(page, pos.x, height - pos.y, checkFont, field.size);
         }
+    }
+    if (field.id === "othersDCheck" && data.othersDG) {
+        drawCheckmark(page, field.x, height - field.y, checkFont, field.size);
+    }
+    const isChecked = data[field.id];
+    if (isChecked === true) {
+        drawCheckmark(page, field.x, height - field.y, checkFont, field.size);
     }
 }
 
